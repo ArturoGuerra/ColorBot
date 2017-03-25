2cc3b1888e50c7099ad3d3888988cf6ab342e7c7
Arturo Guerra <arturoguerra360@gmail.com>
2017-03-25 08:25:36 -0600

    Better code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ColorBot
+Best bot ever created !!!!
+### How to run
+`python colorbot.py <token>`
+
+# Disclaimer
+I'm not responsible if your account get banned for using this bot
--- colorbot.py
+++ colorbot.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
-import discord
+#!/usr/bin/env python3
+import sys
 import json
-import logging
 import random
+import discord
+import logging
 import asyncio
 client = discord.Client()
 logging.basicConfig(level=logging.INFO)
@@ -12,51 +13,35 @@ async def on_ready():
     logger.info("Username: {}".format(client.user.name))
     logger.info("User Discriminator: {}".format(client.user.discriminator))
     logger.info("User ID: {}".format(client.user.id))
-async def rand_color():
-    await client.wait_until_ready()
-    numb = random.randint(1,15)
-    if numb == 1:
-        color = 0x9400D3
-    if numb == 2:
-        color = 0x4B0082
-    if numb == 3:
-        color = 0x0000FF
-    if numb == 4:
-        color = 0x00FF00
-    if numb == 5:
-        color = 0xFFFF00
-    if numb == 6:
-        color = 0xFF7F00
-    if numb == 7:
-        color = 0xFF0000
-    if numb == 8:
-        color = 0xf442bc 
-    if numb == 9:
-        color = 0xf4eb42
-    if numb == 10:
-        color = 0x42d4f4
-    if numb == 11:
-        color = 0x6bf442
-    if numb == 12:
-        color = 0x4265f4
-    if numb == 13:
-        color = 0xf44253
-    if numb == 14:
-        color = 0x42f4e8
-    if numb == 15:
-        color = 0xf49542
-    color = discord.Colour(color)
-    for server in client.servers:
+    while True:
+        color =
+             (
+             0x9400D3,
+             0x4B0082,
+             0x0000FF,
+             0x00FF00,
+             0xFFFF00,
+             0xFF7F00,
+             0xFF0000,
+             0xf442bc,
+             0xf4eb42,
+             0x42d4f4,
+             0x6bf442,
+             0x4265f4,
+             0xf44253,
+             0x42f4e8,
+             0xf49542,
+             )
+        numb = random.randint(0, len(colors) -1)
+        color = discord.Colour(colors[numb])
+        server = discord.utils.get(client.servers, id=config['server'])
         for role in server.roles:
             if not role == server.default_role:
                 try:
                     await client.edit_role(server, role, colour=color)
                     logger.info("{} has changed color in {}".format(role.name, server.name))
-                    await asyncio.sleep(7)
+                    await asyncio.sleep(5)
                 except:
                     logger.info("Error changing: {} in {}".format(role.name, server.name))
-    await rand_color()
-client.loop.create_task(rand_color())
-with open('config.json', 'r') as f:
-    config = json.load(f)
-client.run(config['token'])
+
+client.run(sys.argv[1])
